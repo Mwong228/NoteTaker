c9487cae8ae38f181cfa27cfa050a725a7894520
Michael Wong <michael.w228@gmail.com>
2021-02-23 23:30:37 -0500

    added comments

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const uuid = require('uuid')
 const PORT = 3000
 
+//loads html pages using public folder 
 app.use(express.static('public'))
 
 app.use(express.urlencoded({extended: true}))
@@ -13,16 +14,19 @@ const dbFile = './db/db.json'
 
 const list = fs.existsSync(dbFile)?JSON.parse(fs.readFileSync(dbFile)):[]
 
+//Calls db.json database file
 app.get('/api/notes', function(req, res){
     res.send(list)
 })
 
+//Writes new entries into the database file
 app.post('/api/notes', function(req, res){
     list.push({id: uuid.v4(), title: req.body.title, text: req.body.text})
     fs.writeFileSync(dbFile, JSON.stringify(list))
     res.redirect('/notes')
 })
 
+//Deletes entries from the database file
 app.delete('/api/notes/:id', function(req, res){
     saved = list.filter(entry => entry.id === parseInt(req.params.id)).indexOf(req.params.id)
     list.splice(saved, 1)
