93a201d01623ef5a7ec878b698dccccbc7593808
Michael Wong <michael.w228@gmail.com>
2021-02-23 22:18:50 -0500

    updated

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const uuid = require('uuid')
 const PORT = 3000
 
 app.use(express.static('public'))
@@ -7,9 +9,21 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-const dbFile = './db/db,json'
+const dbFile = './db/db.json'
 
+const list = fs.existsSync(dbFile)?JSON.parse(fs.readFileSync(dbFile)):[]
 
+app.get('/api/notes', function(req, res){
+    res.sendFile(list)
+})
+
+app.post('/api/notes', function(req, res){
+    list.push({id: uuid(), title: req.body.title, text: req.body.text})
+})
+
+app.delete('/api/notes/:id', function(req, res){
+
+})
 
 app.listen(PORT, function(){
     console.log(`Serving notes on PORT ${PORT}`)
